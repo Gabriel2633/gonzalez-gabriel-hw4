147accf44e57eeeb6fe125672f75c28facd99f22
Gabriel Gonzalez <ge.gonzalez13@uniandes.edu.co>
2018-11-19 00:29:55 -0500

    agregar lib cmath y calcular x y x_prime con solo las coordinadas/velocidad de x

--- ODE.cpp
+++ ODE.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -13,11 +14,13 @@ using namespace std;
 int g=10;   // gravedad
 double c=0.2; // esta es la friccion
 double m=0.2; // masa
+double PI=3.14159265359; // PI
 
-double funcion_prime(double t, double X,double X_prime);
-double funcion_prime2(double t, double X, double X_prime);
+double funcion_xprime(double t, double x,double x_prime);
+double funcion_xprime2(double t, double x, double x_prime, double V);
 
-int main() {
+int main() 
+{
      cout<< "testing "<< g <<endl;
 
 
@@ -26,34 +29,40 @@ int main() {
     double h=0.01;
     double t=0;
     double X=0;
+    double x=0;
+    double y=0;
     int ang = 45; //angulo
-    double V = 300;
+    double V0=300;
+    double V = V0;
     int n_steps = 100;
     int i=0;
 
-    for(i=0;i<n_steps;i++) {
+    for(i=0;i<n_steps;i++) 
+    {
+        double x_prime = V*cos(PI*ang/180);
+        double y_prime = V*sin(PI*ang/180);
 
-        double k1=funcion_prime(t,X, V);
-        double k1prime=funcion_prime2(t,X, V);
+        double k1x=funcion_xprime(t,x,x_prime);
+        double k1xprime=funcion_xprime2(t,x,x_prime,V);
         
-        double k2=funcion_prime(t+0.5*h,X+0.5*k1*h,V+0.5*k1prime*h);
-        double k2prime=funcion_prime2(t+0.5*h,X+0.5*k1*h, V+0.5*k1prime*h);
+        double k2=funcion_xprime(t+0.5*h,x+0.5*k1x*h,V+0.5*k1xprime*h);
+        double k2prime=funcion_xprime2(t+0.5*h,x+0.5*k1x*h, V+0.5*k1xprime*h,V);
 
-        double k3=funcion_prime(t+0.5*h,X+0.5*k2*h,V+0.5*k2prime*h);
-        double k3prime=funcion_prime2(t+0.5*h,X+0.5*k2*h, V+0.5*k2prime*h);
+        double k3=funcion_xprime(t+0.5*h,x+0.5*k2*h,V+0.5*k2prime*h);
+        double k3prime=funcion_xprime2(t+0.5*h,x+0.5*k2*h, V+0.5*k2prime*h,V);
 
-        double k4=funcion_prime(t+h,X+k3*h,V+k3prime*h);
-        double k4prime=funcion_prime2(t+h,X+k3*h, V+k3prime*h);
+        double k4=funcion_xprime(t+h,x+k3*h,V+k3prime*h);
+        double k4prime=funcion_xprime2(t+h,x+k3*h, V+k3prime*h,V);
 
-        double promedio_k=(1.0/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4);
-        double promedio_kprime=(1.0/6.0)*(k1prime + 2.0*k2prime + 2.0*k3prime + k4prime);
+        double promedio_k=(1.0/6.0)*(k1x + 2.0*k2 + 2.0*k3 + k4);
+        double promedio_kprime=(1.0/6.0)*(k1xprime + 2.0*k2prime + 2.0*k3prime + k4prime);
 
-        X = X + h*promedio_k;
+        x = x + h*promedio_k;
         V = V + h*promedio_kprime;
         t=t+h;
 
         cout<< "Tiempo recorrido: "<< t <<endl;
-        cout<< "Distancia recorrida: "<< X <<endl;
+        cout<< "Distancia recorrida: "<< x <<endl;
         cout<< "Velocidad: "<< V <<endl;
     }
 
@@ -61,12 +70,14 @@ int main() {
 
 // double funcionprime(t,X,X_prime);
 
-double funcion_prime(double t, double X,double X_prime){
-    return X_prime;
+double funcion_xprime(double t, double x,double x_prime)
+{
+    return x_prime;
 }
 
-double funcion_prime2(double t, double X, double X_prime){
-    return -g-c*X_prime*X_prime/m;
+double funcion_xprime2(double t, double x, double x_prime, double V)
+{
+    return -0-c*V*x_prime/m;
 }
 
 // double runge_kutta(){
@@ -105,10 +116,10 @@ double funcion_prime2(double t, double X, double X_prime){
 
 // // separacion de la derivada y la solucion
 
-// double v(double vx, double vy)
-// {
-//     return vx*vx+vy*vy;
-// }
+double V(double vx, double vy)
+{
+    return pow(vx*vx+vy*vy,0.5);
+}
 
 // double funcion1x(double t, double x, double dx, double y, double dy, double m, double c)
 // {
