d0c5b562594d7776deafe7ba6ebfb5d3b0a3bdf9
Gabriel Gonzalez <ge.gonzalez13@uniandes.edu.co>
2018-11-19 00:54:31 -0500

    agregar y y y_prime

--- ODE.cpp
+++ ODE.cpp
@@ -18,12 +18,12 @@ double PI=3.14159265359; // PI
 
 double funcion_xprime(double t, double x,double x_prime);
 double funcion_xprime2(double t, double x, double x_prime, double V);
+double funcion_yprime(double t, double y,double y_prime);
+double funcion_yprime2(double t, double y, double y_prime, double V);
+double funcion_V(double vx, double vy);
 
 int main() 
 {
-     cout<< "testing "<< g <<endl;
-
-
     // condiciones iniciales
 
     double h=0.01;
@@ -34,35 +34,55 @@ int main()
     int ang = 45; //angulo
     double V0=300;
     double V = V0;
-    int n_steps = 100;
+    int n_steps = 100000;
     int i=0;
 
     for(i=0;i<n_steps;i++) 
     {
         double x_prime = V*cos(PI*ang/180);
-        double y_prime = V*sin(PI*ang/180);
 
         double k1x=funcion_xprime(t,x,x_prime);
         double k1xprime=funcion_xprime2(t,x,x_prime,V);
         
-        double k2=funcion_xprime(t+0.5*h,x+0.5*k1x*h,V+0.5*k1xprime*h);
-        double k2prime=funcion_xprime2(t+0.5*h,x+0.5*k1x*h, V+0.5*k1xprime*h,V);
+        double k2x=funcion_xprime(t+0.5*h,x+0.5*k1x*h,V+0.5*k1xprime*h);
+        double k2xprime=funcion_xprime2(t+0.5*h,x+0.5*k1x*h, V+0.5*k1xprime*h,V);
 
-        double k3=funcion_xprime(t+0.5*h,x+0.5*k2*h,V+0.5*k2prime*h);
-        double k3prime=funcion_xprime2(t+0.5*h,x+0.5*k2*h, V+0.5*k2prime*h,V);
+        double k3x=funcion_xprime(t+0.5*h,x+0.5*k2x*h,V+0.5*k2xprime*h);
+        double k3xprime=funcion_xprime2(t+0.5*h,x+0.5*k2x*h, V+0.5*k2xprime*h,V);
 
-        double k4=funcion_xprime(t+h,x+k3*h,V+k3prime*h);
-        double k4prime=funcion_xprime2(t+h,x+k3*h, V+k3prime*h,V);
+        double k4x=funcion_xprime(t+h,x+k3x*h,V+k3xprime*h);
+        double k4xprime=funcion_xprime2(t+h,x+k3x*h, V+k3xprime*h,V);
 
-        double promedio_k=(1.0/6.0)*(k1x + 2.0*k2 + 2.0*k3 + k4);
-        double promedio_kprime=(1.0/6.0)*(k1xprime + 2.0*k2prime + 2.0*k3prime + k4prime);
+        double y_prime = V*sin(PI*ang/180);
 
-        x = x + h*promedio_k;
-        V = V + h*promedio_kprime;
+        double k1y=funcion_yprime(t,y,y_prime);
+        double k1yprime=funcion_yprime2(t,y,y_prime,V);
+        
+        double k2y=funcion_yprime(t+0.5*h,y+0.5*k1y*h,V+0.5*k1yprime*h);
+        double k2yprime=funcion_yprime2(t+0.5*h,y+0.5*k1y*h, V+0.5*k1yprime*h,V);
+
+        double k3y=funcion_yprime(t+0.5*h,y+0.5*k2y*h,V+0.5*k2yprime*h);
+        double k3yprime=funcion_yprime2(t+0.5*h,y+0.5*k2y*h, V+0.5*k2yprime*h,V);
+
+        double k4y=funcion_yprime(t+h,y+k3y*h,V+k3yprime*h);
+        double k4yprime=funcion_yprime2(t+h,y+k3y*h, V+k3yprime*h,V);
+
+        double promedio_kx=(1.0/6.0)*(k1x + 2.0*k2x + 2.0*k3x + k4x);
+        double promedio_kxprime=(1.0/6.0)*(k1xprime + 2.0*k2xprime + 2.0*k3xprime + k4xprime);
+
+        double promedio_ky=(1.0/6.0)*(k1y + 2.0*k2y + 2.0*k3y + k4y);
+        double promedio_kyprime=(1.0/6.0)*(k1yprime + 2.0*k2yprime + 2.0*k3yprime + k4yprime);
+
+        x = x + h*promedio_kx;
+        y = y + h*promedio_ky;
+        x_prime = x_prime + h*promedio_kxprime;
+        y_prime = y_prime + h*promedio_kyprime;
+        V = funcion_V(x_prime,y_prime);
         t=t+h;
 
-        cout<< "Tiempo recorrido: "<< t <<endl;
-        cout<< "Distancia recorrida: "<< x <<endl;
+        cout<< "Tiempo: "<< t <<endl;
+        cout<< "Distancia: "<< x <<endl;
+        cout<< "y : "<< y <<endl;
         cout<< "Velocidad: "<< V <<endl;
     }
 
@@ -80,6 +100,22 @@ double funcion_xprime2(double t, double x, double x_prime, double V)
     return -0-c*V*x_prime/m;
 }
 
+double funcion_yprime(double t, double y,double y_prime)
+{
+    return y_prime;
+}
+
+double funcion_yprime2(double t, double y, double y_prime, double V)
+{
+    return -g-c*V*y_prime/m;
+}
+
+double funcion_V(double vx, double vy)
+{
+    return pow(vx*vx+vy*vy,0.5);
+}
+
+
 // double runge_kutta(){
 
 // }
@@ -91,13 +127,13 @@ double funcion_xprime2(double t, double x, double x_prime, double V)
 //     for(i=0;i<n_pts;i++) {
 //         double k1=h*funcionprime(x[i-1],y[i-1]);
     
-//         double k2=h*funcionprime(x[i-1]+0.5*h,y[i-1]+0.5*k1);
+//         double k2x=h*funcionprime(x[i-1]+0.5*h,y[i-1]+0.5*k1);
     
-//         double k3=h*funcionprime(x[i-1]+0.5*h,y[i-1]+0.5*k2);
+//         double k3x=h*funcionprime(x[i-1]+0.5*h,y[i-1]+0.5*k2x);
     
-//         double k4=h*funcionprime(x[i-1]+h,y[i-1]+k3);
+//         double k4x=h*funcionprime(x[i-1]+h,y[i-1]+k3x);
     
-//         double promedio_k = (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0;
+//         double promedio_k = (k1 + 2.0*k2x + 2.0*k3x + k4x)/6.0;
     
 //         t[i]=x[i-1]+h;
 
@@ -116,10 +152,7 @@ double funcion_xprime2(double t, double x, double x_prime, double V)
 
 // // separacion de la derivada y la solucion
 
-double V(double vx, double vy)
-{
-    return pow(vx*vx+vy*vy,0.5);
-}
+
 
 // double funcion1x(double t, double x, double dx, double y, double dy, double m, double c)
 // {
